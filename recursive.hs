1b4c3e9fc2681e4340de90063fcbeb7718ec4b87
Oren Koriat <oren@cynerio.co>
2018-11-08 16:21:33 +0200

    added fac

--- recursive.hs
+++ recursive.hs
@@ -1,3 +1,9 @@
-maxi :: (Ord n) => [n] -> n; maxi [n] = n; maxi (x:xs) = max x (maxi xs)
+maxi :: (Ord n) => [n] -> n
+maxi [n] = n
+maxi (x:xs) = max x (maxi xs)
 
-main = print $ "maximum [7, 23, 3, 18]: " ++ show (maxi [7, 23, 3, 18])
+fac 0 = 1
+fac n = n * fac(n - 1)
+
+main = print $ "maximum [7, 23, 3, 18]: " ++ show (maxi [7, 23, 3, 18]) 
+
