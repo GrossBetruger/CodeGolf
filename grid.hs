53550d2eff0b719301b25f033f47ff03d12bc77d
koi <koriatoren@gmail.com>
2018-11-30 17:09:21 +0200

    first column

--- grid.hs
+++ grid.hs
@@ -1,11 +1,11 @@
 
 
-naturals = [1..100]
+naturals = [1..102]
 
 layer xs
   | length xs < 2 = []
   | otherwise = [head xs + (head . tail) xs] ++ layer(drop 1 xs)
 
-grid xs = [(head . layer) xs] ++ grid (layer xs)
+firstColumn xs = [(head . layer) xs] ++ firstColumn (layer xs)
 
-main = print (take 10 (grid naturals))
+main = print (take 10 (firstColumn naturals))
